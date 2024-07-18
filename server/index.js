37482e89f25d29ab421379909e5c95d96ff4171b
MannaiSana <mannaisana941@gmail.com>
2024-07-18 14:11:01 +0200

    Fitness-Project

---
+++ server/index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./routes/User.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); // for form data
+
+app.use("/api/user/", UserRoutes);
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello developers from GFG",
+  });
+});
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(
+      "mongodb+srv://mannaisana941:xNKDW7KyQ5BtQCSp@cluster0.vyda6ai.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("failed to connect with mongo");
+      console.error(err);
+    });
+};
+
+const startServer = async () => {
+  try {
+    app.listen(8080, () => {
+      console.log("Server started on port 8080");
+      connectDB();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
